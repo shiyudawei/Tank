0ee42381e45c9a367467eb80b8560af513a44a00
Dawei Yu <shiyudawei@163.com>
2023-02-18 20:38:17 +0800

    增加键盘监听

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.mashibing.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -12,6 +14,8 @@ public class TankFrame extends Frame {
         setResizable(false);
         setTitle("Tank War");
         setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -28,6 +32,20 @@ public class TankFrame extends Frame {
         g.fillRect(x, y, 50, 50);
         x += 10;
         y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            System.out.println("key pressed");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            super.keyReleased(e);
+            System.out.println("key released");
+        }
     }
 
 }
