858da2414175b04f148766c414b5e064192e60d2
Dawei Yu <shiyudawei@163.com>
2023-02-18 20:09:37 +0800

    从Frame类继承，并且重写paint方法，画出一个黑方块

--- src/com/mashibing/tank/T.java
+++ src/com/mashibing/tank/T.java
@@ -6,21 +6,6 @@ import java.awt.event.WindowEvent;
 
 public class T {
     public static void main(String[] args) {
-        Frame f = new Frame();
-
-        f.setSize(800, 600);
-        f.setResizable(false);
-        f.setTitle("Tank War");
-
-        //匿名内部类
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-                System.exit(0);
-            }
-        });
-
-        f.setVisible(true);
+       TankFrame tk = new TankFrame();
     }
 }
---
+++ src/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,28 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    public TankFrame() throws HeadlessException {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.fillRect(200, 200, 50, 50);
+    }
+
+}
