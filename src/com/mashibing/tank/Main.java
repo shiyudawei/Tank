eccb8de25cab4296067ab01e276b6a6e4f1b60b5
Dawei Yu <shiyudawei@163.com>
2023-03-04 21:25:23 +0800

    配置文件config。定义PropertyMgr使程序更加灵活

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -3,8 +3,10 @@ package com.mashibing.tank;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
        TankFrame tf = new TankFrame();
+
+       int initTankCount = Integer.parseInt((String)PropertyMgr.get("tankCounts"));
        //初始化敌方坦克
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < initTankCount; i++) {
             Tank t = new Tank(50 + i*80, 100, Dir.DOWN, tf, Group.BAD);
             t.setMoving(true);
             tf.tanks.add(t);
